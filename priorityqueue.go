5da2d740665412f91fed02d9d2c158ec90839a14
abhishek <abhishekbagchi@ymail.com>
2020-06-07 14:58:16 +0100

    Bugfix in priority queue implimentation

--- priorityqueue.go
+++ priorityqueue.go
@@ -1,30 +1,80 @@
 package priorityqueue
 
 import (
-	_ "container/heap"
+	"container/heap"
+	"errors"
 )
 
 //An Element is what's held by the priority queue
 type Element struct {
-	value    *interface{}
+	value    interface{}
 	priority int
 }
 
+type elements []*Element
+
 type PriorityQueue struct {
-	queue []*Element
+	queue pq_internal
+}
+
+func New(typ PriorityQueueType) *PriorityQueue {
+	pq := &PriorityQueue{queue: pq_internal{typ: typ}}
+	heap.Init(&pq.queue)
+	return pq
+}
+
+type pq_internal struct {
+	queue elements
 	typ   PriorityQueueType
 }
 
-func (pq PriorityQueue) Len() int { return len(pq.queue) }
+func (pq pq_internal) Len() int { return len(pq.queue) }
 
-func (pq PriorityQueue) Less(i, j int) bool {
+func MinQueueFunc(pi, pj int) bool {
+	return pi < pj
+}
+
+func MaxQueueFunc(pi, pj int) bool {
+	return pi > pj
+}
+
+func (pq pq_internal) Less(i, j int) bool {
 	if pq.typ == MinQueue {
-		return pq.queue[i].priority < pq.queue[j].priority
+		return MinQueueFunc(pq.queue[i].priority, pq.queue[j].priority)
 	} else {
-		return pq.queue[i].priority > pq.queue[j].priority
+		return MaxQueueFunc(pq.queue[i].priority, pq.queue[j].priority)
 	}
 }
 
-func (pq PriorityQueue) Swap(i, j int) {
+func (pq pq_internal) Swap(i, j int) {
 	pq.queue[i], pq.queue[j] = pq.queue[j], pq.queue[i]
+}
+
+func (pq *pq_internal) Push(x interface{}) {
+	element := x.(*Element)
+	pq.queue = append(pq.queue, element)
+}
+
+func (pq *pq_internal) Pop() interface{} {
+	size := len(pq.queue)
+	element := pq.queue[size-1]
+	pq.queue[size-1] = nil //Avoid memory leak
+	pq.queue = pq.queue[0 : size-1]
+	return element
+}
+
+//Len returns the number of elements in the queue
+func (pq *PriorityQueue) Len() int { return pq.queue.Len() }
+
+func (pq *PriorityQueue) Push(value interface{}, priority int) {
+	element := &Element{value, priority}
+	heap.Push(&pq.queue, element)
+}
+
+func (pq *PriorityQueue) Pop() (interface{}, int, error) {
+	if pq.Len() == 0 {
+		return nil, 0, errors.New("No elements to pop")
+	}
+	element := *(heap.Pop(&pq.queue).(*Element))
+	return element.value, element.priority, nil
 }
